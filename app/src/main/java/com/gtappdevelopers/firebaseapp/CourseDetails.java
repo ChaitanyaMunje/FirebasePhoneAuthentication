b602b6fac1021246344af3f49df356b30c9af26e
chaitanyamunje@gmail.com <Munje@1069>
2021-01-04 12:02:01 +0530

    Firebase Firestore Read and Write Operations in Android.

---
+++ app/src/main/java/com/gtappdevelopers/firebaseapp/CourseDetails.java
@@ -0,0 +1,87 @@
+package com.gtappdevelopers.firebaseapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CourseDetails extends AppCompatActivity {
+
+    //creating variabes for our recycler view, array list, adapter, firebase firestore and our progress bar.
+    private RecyclerView courseRV;
+    private ArrayList<Courses> coursesArrayList;
+    private CourseRVAdapter courseRVAdapter;
+    private FirebaseFirestore db;
+    ProgressBar loadingPB;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course_details);
+        //initializing our variables.
+        courseRV = findViewById(R.id.idRVCourses);
+        loadingPB = findViewById(R.id.idProgressBar);
+        //initializing our variable for firebase firestore and getting its instance.
+        db = FirebaseFirestore.getInstance();
+        //creating our new array list
+        coursesArrayList = new ArrayList<>();
+        courseRV.setHasFixedSize(true);
+        courseRV.setLayoutManager(new LinearLayoutManager(this));
+        //adding our array list to our recycler view adapter class.
+        courseRVAdapter = new CourseRVAdapter(coursesArrayList, this);
+        //setting adapter to our recycler view.
+        courseRV.setAdapter(courseRVAdapter);
+        //below line is use to get the data from Firebase FIrestore.
+        //previously we were saving data on a refrence of Vourses
+        //now we will be getting the data from the same refrence.
+
+        db.collection("Courses").get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        //after getting the data we are calling on success method
+                        //and inside this method we are checking if the recieved query snapshot is empty or not.
+                        if (!queryDocumentSnapshots.isEmpty()) {
+                            //if the snapshot is not empty we are hiding our progress bar and adding our data in a list.
+                            loadingPB.setVisibility(View.GONE);
+                            List<DocumentSnapshot> list = queryDocumentSnapshots.getDocuments();
+                            for (DocumentSnapshot d : list) {
+                                //after getting this list we are passing that list to our object class.
+                                Courses c = d.toObject(Courses.class);
+                                //and we will pass this object class inside our arraylist which we have created for recyclcer view.
+                                coursesArrayList.add(c);
+                            }
+                            //after adding the data to recycler view.
+                            // we are calling recycler view notifuDataSetChanged method to notify that data has been changed in recycler view.
+                            courseRVAdapter.notifyDataSetChanged();
+                        } else {
+                            //if the snapshot is empty we are displaying a toast message.
+                            Toast.makeText(CourseDetails.this, "No data found in Database", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //if we donot get any data or any error we are displaying a toast message that we donot get any data
+                Toast.makeText(CourseDetails.this, "Fail to get the data.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
--- app/src/main/java/com/gtappdevelopers/firebaseapp/MainActivity.java
+++ app/src/main/java/com/gtappdevelopers/firebaseapp/MainActivity.java
@@ -1,127 +1,130 @@
 package com.gtappdevelopers.firebaseapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.TextureView;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.android.gms.tasks.TaskExecutors;
-import com.google.firebase.FirebaseException;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.PhoneAuthCredential;
-import com.google.firebase.auth.PhoneAuthProvider;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class MainActivity extends AppCompatActivity {
-    //variable for Firebase Auth
-    private FirebaseAuth mFirebaseAuth;
-    //declaring a const int value which we will be using in Firebase auth.
-    public static final int RC_SIGN_IN = 1;
-    //creating an auth listner for our Firebase auth
-    private FirebaseAuth.AuthStateListener mAuthStateListner;
+import javax.net.ssl.HttpsURLConnection;
 
-    //below is the list which we have created in which we can add the authentication which we have todisplay inside our app.
-    List<AuthUI.IdpConfig> providers = Arrays.asList(
-            //below is the line for adding email and password authentication.
-            new AuthUI.IdpConfig.EmailBuilder().build(),
-            //below line is used for adding google authentication builder in our app.
-            new AuthUI.IdpConfig.GoogleBuilder().build(),
-            //below line is used for adding phone authentication builder in our app.
-            new AuthUI.IdpConfig.PhoneBuilder().build());
+public class MainActivity extends AppCompatActivity {
+
+    //creating variables for our edit text
+    private EditText courseNameEdt, courseDurationEdt, courseDescriptionEdt;
+    //creating variable for button
+    private Button submitCourseBtn, viewCoursesBtn;
+    //creating a strings for storing our values from edittext fields.
+    private String courseName, courseDuration, courseDescription;
+    //creating a variable for firebasefirestore.
+    private FirebaseFirestore db;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //below line is for getting instance for our firebase auth
-        mFirebaseAuth=FirebaseAuth.getInstance();
-        //below line is used for calling auth listner for oue Firebase authentication.
-        mAuthStateListner=new FirebaseAuth.AuthStateListener() {
-            @SuppressLint("ResourceType")
+        //getting our instance from Firebase Firestore.
+        db=FirebaseFirestore.getInstance();
+        //initializing our edittext and buttons
+        courseNameEdt = findViewById(R.id.idEdtCourseName);
+        courseDescriptionEdt = findViewById(R.id.idEdtCourseDescription);
+        courseDurationEdt = findViewById(R.id.idEdtCourseDuration);
+        submitCourseBtn = findViewById(R.id.idBtnSubmitCourse);
+        viewCoursesBtn = findViewById(R.id.idBtnViewCourses);
+        //adding onclick listner to view data in new activity
+        viewCoursesBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                //we are calling method for on authentication state changed.
-                //below line is used for getting current user which is authenticated previously.
-                FirebaseUser user=firebaseAuth.getCurrentUser();
-                //checking if the user is null or not.
-                if (user!=null)
-                {
-                    //if the user is already authenticated then we will redirect our user to next screen which is our home screen.
-                    //we are redirecting to new screen via an intent.
-                    Intent i=new Intent(MainActivity.this,HomeActivity.class);
-                    startActivity(i);
-                    //we are caling finish method to kill or mainactivity which is displaying our login ui.
-                    finish();
-                }
-                else {
-                    //this method is called when our user is not authenticated previously.
-                    startActivityForResult(
-                            //below line is used for getting our authentication instance.
-                            AuthUI.getInstance()
-                                    //below line is used to create our sign in intet
-                                    .createSignInIntentBuilder()
-                                    //below line is used for adding smart lock for our authentication.
-                                    //smart lock is used to check if the user is authentication through different devices.
-                                    //currently we are disabling it.
-                                    .setIsSmartLockEnabled(false)
-                                    //we are adding different login providers which we have mentioned above in our list.
-                                    // we can add more providers according toour requirement which are avaliable in firebase.
-                                    .setAvailableProviders(providers)
-                                    //below line is for customizing our theme for login screen and set logo method is used for adding logo for our login page.
-                                    .setLogo(R.drawable.gfgimage).setTheme(R.style.Theme)
-                                    //after setting our theme and logo we are calling a build() method to build our login screen.
-                                    .build(),
-                            //and lastly we are passing our const integer which is declared above.
-                            RC_SIGN_IN
-                    );
+            public void onClick(View v) {
+                //opening a new activity on button click
+                Intent i = new Intent(MainActivity.this,CourseDetails.class);
+                startActivity(i);
+            }
+        });
 
+        //adding on click listner for button
+        submitCourseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //getting data from edittext fields.
+                courseName = courseNameEdt.getText().toString();
+                courseDescription = courseDescriptionEdt.getText().toString();
+                courseDuration = courseDurationEdt.getText().toString();
+
+                //validating the text fileds if empty or not.
+                if (TextUtils.isEmpty(courseName)) {
+                    courseNameEdt.setError("Please enter Course Name");
+
+                } else if (TextUtils.isEmpty(courseDescription)) {
+                    courseDescriptionEdt.setError("Please enter Course Description");
+                } else if (TextUtils.isEmpty(courseDuration)) {
+                    courseDurationEdt.setError("Please enter Course Duration");
+                }else{
+                    //calling method to add data to Firebase Firestore.
+                    addDataToFirestore(courseName,courseDescription,courseDuration);
                 }
             }
-        };
+        });
+
 
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        //we are calling our auth listner method on app resume.
-        mFirebaseAuth.addAuthStateListener(mAuthStateListner);
+    private void addDataToFirestore(String courseName,String courseDescription,String courseDuration){
+
+        //creating a collection refrence for our Firebase Firetore database.
+        CollectionReference dbCourses = db.collection("Courses");
+        //adding our data to our courses object class.
+        Courses courses = new Courses(courseName,courseDescription,courseDuration);
+
+        //below method is use to add data to Firebase Firestore.
+        dbCourses.add(courses).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+            @Override
+            public void onSuccess(DocumentReference documentReference) {
+                //after the data addition is succesful
+                //we are displaying a success toast message.
+                Toast.makeText(MainActivity.this, "Your Course has been added to Firebase Firestore", Toast.LENGTH_SHORT).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //this method is called when the data addition process is failed.
+                //displaying a toast message when data addition is failed.
+                Toast.makeText(MainActivity.this, "Fail to add course \n"+e, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        //here we are calling remove auth listner method on stop.
-        mFirebaseAuth.removeAuthStateListener(mAuthStateListner);
-
-    }
-
-
-
-
-
-
 
 }
